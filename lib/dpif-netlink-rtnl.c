13146914bcdfc604002ac33c927f9f02d4bc610f
Eric Garver <e@erig.me>
2017-03-16 18:10:20 -0400

    dpif-netlink-rtnl: add GENEVE creation support
    Creates GENEVE devices using rtnetlink and tunnel metadata.
    
    Co-Authored-by: Thadeu Lima de Souza Cascardo <cascardo@redhat.com>
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@redhat.com>
    Signed-off-by: Eric Garver <e@erig.me>

--- lib/dpif-netlink-rtnl.c
+++ lib/dpif-netlink-rtnl.c
@@ -47,6 +47,15 @@
 #define IFLA_GRE_COLLECT_METADATA 18
 #endif
 
+#ifndef IFLA_GENEVE_MAX
+#define IFLA_GENEVE_MAX 0
+#endif
+#if IFLA_GENEVE_MAX < 10
+#define IFLA_GENEVE_PORT 5
+#define IFLA_GENEVE_COLLECT_METADATA 6
+#define IFLA_GENEVE_UDP_ZERO_CSUM6_RX 10
+#endif
+
 static const struct nl_policy rtlink_policy[] = {
     [IFLA_LINKINFO] = { .type = NL_A_NESTED },
 };
@@ -81,6 +90,12 @@ dpif_netlink_rtnl_vxlan_destroy(const char *name)
 
 static int
 dpif_netlink_rtnl_gre_destroy(const char *name)
+{
+    return dpif_netlink_rtnl_destroy(name);
+}
+
+static int
+dpif_netlink_rtnl_geneve_destroy(const char *name)
 {
     return dpif_netlink_rtnl_destroy(name);
 }
@@ -298,6 +313,113 @@ dpif_netlink_rtnl_gre_create(struct netdev *netdev)
     return dpif_netlink_rtnl_gre_create_kind(netdev, "gretap");
 }
 
+static int
+dpif_netlink_rtnl_geneve_verify(struct netdev *netdev, const char *name,
+                             const char *kind)
+{
+    int err;
+    struct ofpbuf request, *reply;
+    struct ifinfomsg *ifmsg;
+    const struct netdev_tunnel_config *tnl_cfg;
+
+    static const struct nl_policy geneve_policy[] = {
+        [IFLA_GENEVE_COLLECT_METADATA] = { .type = NL_A_FLAG },
+        [IFLA_GENEVE_UDP_ZERO_CSUM6_RX] = { .type = NL_A_U8 },
+        [IFLA_GENEVE_PORT] = { .type = NL_A_U16 },
+    };
+
+    tnl_cfg = netdev_get_tunnel_config(netdev);
+    if (!tnl_cfg) {
+        return EINVAL;
+    }
+
+    ofpbuf_init(&request, 0);
+    nl_msg_put_nlmsghdr(&request, 0, RTM_GETLINK, NLM_F_REQUEST);
+    ofpbuf_put_zeros(&request, sizeof(struct ifinfomsg));
+    nl_msg_put_string(&request, IFLA_IFNAME, name);
+
+    err = nl_transact(NETLINK_ROUTE, &request, &reply);
+    ofpbuf_uninit(&request);
+
+    if (!err) {
+        struct nlattr *rtlink[ARRAY_SIZE(rtlink_policy)];
+        struct nlattr *linkinfo[ARRAY_SIZE(linkinfo_policy)];
+        struct nlattr *geneve[ARRAY_SIZE(geneve_policy)];
+
+        ifmsg = ofpbuf_at(reply, NLMSG_HDRLEN, sizeof *ifmsg);
+        if (!nl_policy_parse(reply, NLMSG_HDRLEN + sizeof *ifmsg,
+                             rtlink_policy, rtlink, ARRAY_SIZE(rtlink_policy))
+            || !nl_parse_nested(rtlink[IFLA_LINKINFO], linkinfo_policy,
+                                linkinfo, ARRAY_SIZE(linkinfo_policy))
+            || strcmp(nl_attr_get_string(linkinfo[IFLA_INFO_KIND]), kind)
+            || !nl_parse_nested(linkinfo[IFLA_INFO_DATA], geneve_policy,
+                                geneve, ARRAY_SIZE(geneve_policy))) {
+            err = EINVAL;
+        }
+        if (!err) {
+            if (!nl_attr_get_flag(geneve[IFLA_GENEVE_COLLECT_METADATA])
+                || 1 != nl_attr_get_u8(geneve[IFLA_GENEVE_UDP_ZERO_CSUM6_RX])
+                || (tnl_cfg->dst_port !=
+                    nl_attr_get_be16(geneve[IFLA_GENEVE_PORT]))) {
+                err = EINVAL;
+            }
+        }
+        ofpbuf_delete(reply);
+    }
+
+    return err;
+}
+
+static int
+dpif_netlink_rtnl_geneve_create_kind(struct netdev *netdev, const char *kind)
+{
+    int err;
+    struct ofpbuf request;
+    size_t linkinfo_off, infodata_off;
+    char namebuf[NETDEV_VPORT_NAME_BUFSIZE];
+    const char *name = netdev_vport_get_dpif_port(netdev,
+                                                  namebuf, sizeof namebuf);
+    struct ifinfomsg *ifinfo;
+    const struct netdev_tunnel_config *tnl_cfg;
+    tnl_cfg = netdev_get_tunnel_config(netdev);
+    if (!tnl_cfg) {
+        return EINVAL;
+    }
+
+    ofpbuf_init(&request, 0);
+    nl_msg_put_nlmsghdr(&request, 0, RTM_NEWLINK,
+                        NLM_F_REQUEST | NLM_F_ACK | NLM_F_CREATE);
+    ifinfo = ofpbuf_put_zeros(&request, sizeof(struct ifinfomsg));
+    ifinfo->ifi_change = ifinfo->ifi_flags = IFF_UP;
+    nl_msg_put_string(&request, IFLA_IFNAME, name);
+    nl_msg_put_u32(&request, IFLA_MTU, UINT16_MAX);
+    linkinfo_off = nl_msg_start_nested(&request, IFLA_LINKINFO);
+    nl_msg_put_string(&request, IFLA_INFO_KIND, kind);
+    infodata_off = nl_msg_start_nested(&request, IFLA_INFO_DATA);
+
+    nl_msg_put_flag(&request, IFLA_GENEVE_COLLECT_METADATA);
+    nl_msg_put_u8(&request, IFLA_GENEVE_UDP_ZERO_CSUM6_RX, 1);
+    nl_msg_put_be16(&request, IFLA_GENEVE_PORT, tnl_cfg->dst_port);
+
+    nl_msg_end_nested(&request, infodata_off);
+    nl_msg_end_nested(&request, linkinfo_off);
+
+    err = nl_transact(NETLINK_ROUTE, &request, NULL);
+    ofpbuf_uninit(&request);
+
+    if (!err && (err = dpif_netlink_rtnl_geneve_verify(netdev, name, kind))) {
+        dpif_netlink_rtnl_geneve_destroy(name);
+    }
+
+    return err;
+}
+
+static int
+dpif_netlink_rtnl_geneve_create(struct netdev *netdev)
+{
+    return dpif_netlink_rtnl_geneve_create_kind(netdev, "geneve");
+}
+
 int
 dpif_netlink_rtnl_port_create(struct netdev *netdev)
 {
@@ -307,6 +429,7 @@ dpif_netlink_rtnl_port_create(struct netdev *netdev)
     case OVS_VPORT_TYPE_GRE:
         return dpif_netlink_rtnl_gre_create(netdev);
     case OVS_VPORT_TYPE_GENEVE:
+        return dpif_netlink_rtnl_geneve_create(netdev);
     case OVS_VPORT_TYPE_NETDEV:
     case OVS_VPORT_TYPE_INTERNAL:
     case OVS_VPORT_TYPE_LISP:
@@ -328,6 +451,7 @@ dpif_netlink_rtnl_port_destroy(const char *name, const char *type)
     case OVS_VPORT_TYPE_GRE:
         return dpif_netlink_rtnl_gre_destroy(name);
     case OVS_VPORT_TYPE_GENEVE:
+        return dpif_netlink_rtnl_geneve_destroy(name);
     case OVS_VPORT_TYPE_NETDEV:
     case OVS_VPORT_TYPE_INTERNAL:
     case OVS_VPORT_TYPE_LISP:
