a5313668b7eb7093ed9701bfbe39da75f00b66ab
Joe Stringer <joe@ovn.org>
2017-05-24 12:02:18 -0700

    dpif: Refactor stub implementation for meter_*().
    Currently the stubs for if a DPIF doesn't support meters are all
    implemented in dpif-netlink. However, it makes sense instead for this
    stub logic to be put in the DPIF layer and just set the pointers to NULL
    in dpif-netlink. This simplifies stubbing out meter support in a new
    upcoming dpif.
    
    Signed-off-by: Joe Stringer <joe@ovn.org>

--- lib/dpif.c
+++ lib/dpif.c
@@ -1891,7 +1891,11 @@ dpif_meter_set(struct dpif *dpif, ofproto_meter_id *meter_id,
 
     COVERAGE_INC(dpif_meter_set);
 
-    error = dpif->dpif_class->meter_set(dpif, meter_id, config);
+    if (dpif->dpif_class->meter_set) {
+        error = dpif->dpif_class->meter_set(dpif, meter_id, config);
+    } else {
+        error = EFBIG;
+    }
     if (!error) {
         VLOG_DBG_RL(&dpmsg_rl, "%s: DPIF meter %"PRIu32" set",
                     dpif_name(dpif), meter_id->uint32);
@@ -1911,7 +1915,11 @@ dpif_meter_get(const struct dpif *dpif, ofproto_meter_id meter_id,
 
     COVERAGE_INC(dpif_meter_get);
 
-    error = dpif->dpif_class->meter_get(dpif, meter_id, stats, n_bands);
+    if (dpif->dpif_class->meter_get) {
+        error = dpif->dpif_class->meter_get(dpif, meter_id, stats, n_bands);
+    } else {
+        error = EFBIG;
+    }
     if (!error) {
         VLOG_DBG_RL(&dpmsg_rl, "%s: DPIF meter %"PRIu32" get stats",
                     dpif_name(dpif), meter_id.uint32);
@@ -1934,7 +1942,11 @@ dpif_meter_del(struct dpif *dpif, ofproto_meter_id meter_id,
 
     COVERAGE_INC(dpif_meter_del);
 
-    error = dpif->dpif_class->meter_del(dpif, meter_id, stats, n_bands);
+    if (dpif->dpif_class->meter_del) {
+        error = dpif->dpif_class->meter_del(dpif, meter_id, stats, n_bands);
+    } else {
+        error = EFBIG;
+    }
     if (!error) {
         VLOG_DBG_RL(&dpmsg_rl, "%s: DPIF meter %"PRIu32" deleted",
                     dpif_name(dpif), meter_id.uint32);
