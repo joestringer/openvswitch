33e0199ba43270e8fb1263ae832ca59434ff2bd9
Joe Stringer <joestringernz@gmail.com>
2017-06-02 11:37:17 -0700

    XXX: Fix bpf/automake

--- bpf/automake.mk
+++ bpf/automake.mk
@@ -3,19 +3,21 @@ bpf_headers = \
 	bpf/api.h \
 	bpf/datapath.h \
 	bpf/odp-bpf.h \
-    bpf/ovs-p4.h \
-    bpf/helpers.h \
-    bpf/openvswitch.h \
-    bpf/maps.h \
-    bpf/parser.h \
-    bpf/lookup.h \
-    bpf/action.h \
-    bpf/deparser.h \
-    bpf/generated_headers.h
+	bpf/ovs-p4.h \
+	bpf/helpers.h \
+	bpf/openvswitch.h \
+	bpf/maps.h \
+	bpf/parser.h \
+	bpf/lookup.h \
+	bpf/action.h \
+	bpf/deparser.h \
+	bpf/generated_headers.h
+bpf_extra = \
+	bpf/compile.sh \
+	bpf/ovs-proto.p4
 
 bpf_CFLAGS = $(AM_CFLAGS)
 bpf_CFLAGS += -target bpf -D__NR_CPUS__=$(shell nproc) -O2 -Wall -Werror
-#bpf_CFLAGS += -D__NR_CPUS__=$(shell nproc) -O2 -Wall -Werror -emit-llvm -g
 bpf_CFLAGS += -I$(top_builddir)/include -I$(top_srcdir)/include
 
 dist_sources = $(bpf_sources)
@@ -25,16 +27,13 @@ build_headers = $(dist_headers))
 build_objects = $(patsubst %.c,%.o,$(build_sources))
 
 bpf: $(build_objects)
-bpf/%.o: $(srcdir)/bpf/%.c
+bpf/datapath.o: $(bpf_sources) $(bpf_headers)
 	$(MKDIR_P) $(dir $@)
 	@which clang >/dev/null 2>&1 || \
 		(echo "Unable to find clang, Install clang (>=3.7) package"; exit 1)
 	clang $(bpf_CFLAGS) -c $< -o $@
 
-#clang $(bpf_CFLAGS) -c $< -o -| llc -march=bpf -filetype=obj -o $@
-#llvm-objdump -S -no-show-raw-insn $@ > $@.objdump
-
-EXTRA_DIST += $(dist_sources) $(dist_headers) bpf/ovs-proto.p4 bpf/compile.sh
+EXTRA_DIST += $(dist_sources) $(dist_headers) $(bpf_extra)
 if HAVE_BPF
 dist_bpf_DATA += $(build_objects)
 endif
