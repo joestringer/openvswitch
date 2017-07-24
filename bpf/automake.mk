169dd3838bb7a55e80bf4fdf7d7edb8eb8419301
Joe Stringer <joe@ovn.org>
2017-07-24 14:37:38 -0700

    bpf: Import conntrack implementation from Cilium.
    Signed-off-by: Joe Stringer <joe@ovn.org>

--- bpf/automake.mk
+++ bpf/automake.mk
@@ -1,7 +1,27 @@
-bpf_headers = \
+bpf_sources = bpf/datapath.c
+bpf_cilium_headers = \
 	bpf/api.h \
+	bpf/bpf_features.h \
+	bpf/common.h \
+	bpf/conntrack.h \
+	bpf/csum.h \
+	bpf/ipv4.h \
+	bpf/ipv6.h \
+	bpf/l4.h \
+	bpf/utils.h
+bpf_headers = \
+	bpf/datapath.h \
+	bpf/odp-bpf.h \
+	bpf/ovs-p4.h \
+	bpf/helpers.h \
+	bpf/openvswitch.h \
+	bpf/maps.h \
+	bpf/parser.h \
+	bpf/lookup.h \
+	bpf/action.h \
+	bpf/deparser.h \
 	bpf/generated_headers.h \
-	bpf/odp-bpf.h
+	$(bpf_cilium_headers)
 bpf_extra = \
 	bpf/compile.sh \
 	bpf/ovs-proto.p4
